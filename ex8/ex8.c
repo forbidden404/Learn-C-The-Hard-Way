93a2c56bbb3b21b8cda50a0ed20c52c8b09f3571
Francisco Soares <francisco.ssn94@gmail.com>
2019-03-09 21:30:54 -0300

    Extra Credits - Exercise 8

--- ex8/ex8.c
+++ ex8/ex8.c
@@ -5,11 +5,11 @@ int main(int argc, char *argv[])
     int i = 0;
 
     if (argc == 1) {
-        printf("You only have one argument. You suck.\n");
+        printf("You have no arguments. You suck.\n");
     } else if (argc > 1 && argc < 4) {
         printf("Here's your arguments:\n");
 
-        for (i = 0; i < argc; i++) {
+        for (i = 1; i < argc; i++) {
             printf("%s ", argv[i]);
         }
         printf("\n");
