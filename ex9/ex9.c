263e43a6a1d63cdceaaef665cdba939de3d97bf2
Francisco Soares <francisco.ssn94@gmail.com>
2019-03-09 21:52:33 -0300

    Extra Credits - Exercise 9

--- ex9/ex9.c
+++ ex9/ex9.c
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[])
 {
-    int i = 0;
-    while (i < 25) {
+    int i = 25;
+    while (i >= 0) {
         printf("%d", i);
-        i++;
+        i--;
     }
 
     // need this to add a final newline
